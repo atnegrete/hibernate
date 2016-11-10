81e72434e7b16779f67805a88e3e202bb0c410cb
chozenelite <chozenalan123@gmail.com>
2016-11-10 11:28:06 -0600

    set up complete

--- src/p2b/MainApp.java
+++ src/p2b/MainApp.java
@@ -1,5 +1,32 @@
 package p2b;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 public class MainApp {
+	
+	@SuppressWarnings({ "unchecked" })
+	public static void main(String[] args) throws Exception{
+		SessionFactory sessionFactory = null;
+		
+		final  StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+		try
+		{
+			Configuration configuration = new Configuration();
+			sessionFactory = configuration.configure().buildSessionFactory(registry);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+		
+		Session session = sessionFactory.openSession();
+		
+		//TODO: Run all queries. And create mysql tables.
+	}
 
 }
